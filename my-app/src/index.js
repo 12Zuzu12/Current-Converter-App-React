09c2d5440deef78eb74b2fddacbb3faa876168e3
Radgab <radgab712@gmail.com>
2023-05-14 00:13:56 +0300

    add

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { Layout } from 'antd';
-// import 'antd/dist/reset.css';
-// import { Switch } from 'react-router-dom';
-
-
-// const rootElement = document.getElementById('root');
-
-// ReactDOM.createRoot(rootElement).render(
-//   <Router>
-//     <Routes>
-//     <Route exact path="/" component={App} />
-//     </Routes>
-//   </Router>
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -26,12 +5,9 @@ import App from './App';
 import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Layout } from 'antd';
 import 'antd/dist/reset.css';
-import { Switch } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
-
 ReactDOM.render(
   <Router>
     <Routes>
@@ -42,30 +18,4 @@ ReactDOM.render(
   rootElement
 );
 
-// ReactDOM.render(
-//   <Router>
-//     <Layout>
-//     <Routes>
-//         <Route exact path="/" component={App} />
-//         {/* Добавьте другие маршруты здесь */}
-//         </Routes>
-//     </Layout>
-//   </Router>,
-//   document.getElementById('root')
-// );
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-  
-//     <App />
-  
-//   </React.StrictMode>
-// );
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
