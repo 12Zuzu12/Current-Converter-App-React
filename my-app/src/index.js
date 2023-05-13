726eacbd4aa23bbd0237ed6865cb638be959f5dc
Radgab <radgab712@gmail.com>
2023-05-14 00:09:48 +0300

    add

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,69 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { Layout } from 'antd';
+// import 'antd/dist/reset.css';
+// import { Switch } from 'react-router-dom';
+
+
+// const rootElement = document.getElementById('root');
+
+// ReactDOM.createRoot(rootElement).render(
+//   <Router>
+//     <Routes>
+//     <Route exact path="/" component={App} />
+//     </Routes>
+//   </Router>
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from 'antd';
+import 'antd/dist/reset.css';
+import { Switch } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<App />} />
+      <Route exact path="/about" element={<App2 />} />
+    </Routes>
+  </Router>,
+  rootElement
 );
+
+// ReactDOM.render(
+//   <Router>
+//     <Layout>
+//     <Routes>
+//         <Route exact path="/" component={App} />
+//         {/* Добавьте другие маршруты здесь */}
+//         </Routes>
+//     </Layout>
+//   </Router>,
+//   document.getElementById('root')
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+  
+//     <App />
+  
+//   </React.StrictMode>
+// );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
